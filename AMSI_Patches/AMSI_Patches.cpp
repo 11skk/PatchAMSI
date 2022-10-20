10b98c7b3f5f9fd692bc247c0729dbae660ef62c
D1rkMtr <110354855+D1rkMtr@users.noreply.github.com>
2022-10-20 13:57:37 +0100

    Update AMSI_Patches.cpp

--- AMSI_Patches/AMSI_Patches.cpp
+++ AMSI_Patches/AMSI_Patches.cpp
@@ -4,49 +4,6 @@
 using namespace std;
 
 
-typedef struct _UNICODE_STRING
-{
-    USHORT Length;
-    USHORT MaximumLength;
-    PWSTR  Buffer;
-} UNICODE_STRING, * PUNICODE_STRING;
-
-typedef struct _PS_ATTRIBUTE
-{
-    ULONG  Attribute;
-    SIZE_T Size;
-    union
-    {
-        ULONG Value;
-        PVOID ValuePtr;
-    } u1;
-    PSIZE_T ReturnLength;
-} PS_ATTRIBUTE, * PPS_ATTRIBUTE;
-
-typedef struct _OBJECT_ATTRIBUTES
-{
-    ULONG           Length;
-    HANDLE          RootDirectory;
-    PUNICODE_STRING ObjectName;
-    ULONG           Attributes;
-    PVOID           SecurityDescriptor;
-    PVOID           SecurityQualityOfService;
-} OBJECT_ATTRIBUTES, * POBJECT_ATTRIBUTES;
-
-typedef struct _PS_ATTRIBUTE_LIST
-{
-    SIZE_T       TotalLength;
-    PS_ATTRIBUTE Attributes[1];
-} PS_ATTRIBUTE_LIST, * PPS_ATTRIBUTE_LIST;
-
-EXTERN_C NTSTATUS NtAllocateVirtualMemory(
-    IN HANDLE ProcessHandle,
-    IN OUT PVOID* BaseAddress,
-    IN ULONG ZeroBits,
-    IN OUT PSIZE_T RegionSize,
-    IN ULONG AllocationType,
-    IN ULONG Protect);
-
 EXTERN_C NTSTATUS NtProtectVirtualMemory(
     IN HANDLE ProcessHandle,
     IN OUT PVOID* BaseAddress,
@@ -61,18 +18,7 @@ EXTERN_C NTSTATUS NtWriteVirtualMemory(
     IN SIZE_T NumberOfBytesToWrite,
     OUT PSIZE_T NumberOfBytesWritten OPTIONAL);
 
-EXTERN_C NTSTATUS NtCreateThreadEx(
-    OUT PHANDLE ThreadHandle,
-    IN ACCESS_MASK DesiredAccess,
-    IN POBJECT_ATTRIBUTES ObjectAttributes OPTIONAL,
-    IN HANDLE ProcessHandle,
-    IN PVOID StartRoutine,
-    IN PVOID Argument OPTIONAL,
-    IN ULONG CreateFlags,
-    IN SIZE_T ZeroBits,
-    IN SIZE_T StackSize,
-    IN SIZE_T MaximumStackSize,
-    IN PPS_ATTRIBUTE_LIST AttributeList OPTIONAL);
+
 
 
 void patchAMSI(HANDLE& hProc, int PatchNbr) {
